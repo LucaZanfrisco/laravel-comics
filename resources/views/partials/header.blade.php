f1a51898b3cfb6d72e5e8c60fa0172e3e7e08ae6
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-02 18:22:40 +0200

    Header done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,25 @@
 <header> 
     
-    <div class=>
-        <div class="header-up">
-            <div>DC POWER VISA </div>
-            <div>ADDITIONAL DC SITES &triangledown; </div>
+        <div>
+            <div class="container header-up">
+                <div>ADDITIONAL DC SITES &triangledown; </div>
+                <div>DC POWER VISA </div>
+            </div>
         </div>
-        <nav class="d-flex justify-content-center align-items-center">
-            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo-dc" class="img-fluid me-5">
-            <ul class="d-flex justify-content-between list-unstyled gap-3">
-                @foreach ($nav as $item)
-                    <li><a href="#">{{ $item }}</a></li>
-                @endforeach
-            </ul>
-            <div>
-                <input type="text">
+        <nav>
+            <div class="container d-flex align-items-center justify-content-between">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo-dc" class="img-fluid">
+                <ul class="d-flex justify-content-between list-unstyled gap-4">
+                    @foreach ($nav as $item)
+                        <li><a href="#">{{ $item }}</a></li>
+                    @endforeach
+                </ul>
+                <div>
+                    <input type="text" placeholder="Search" class="search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
             </div>
         </nav>
-    </div>
     <div class="jumbotron">
 
     </div>
